1c306c7423649a3d5319092833d7b3028882ec07
robert1025 <robert1025@gmail.com>
2026-02-09 23:07:56 +0100

    rebuild finish. all work fine

--- sketch.js
+++ sketch.js
@@ -7,11 +7,15 @@ let tubeSize = null;
 let plotSize = null;
 
 let i = 0;
-let count = 1;
+let count = 0;
 
 let startTime;
 let newTime = 0;
 let elapsed = 0;
+
+let speedPxPerSec = 50;
+let bubbleOffset = 0;
+let lastBubbleY = null;
 
 // logic
 let startAnimation = false;
@@ -33,7 +37,6 @@ let windowHeight = document.documentElement.clientHeight;
 const startBtn = document.querySelector('#startBtn');
 startBtn.addEventListener('click', () => {
     startAnimation = true;
-    // startTime = millis();
 });
 
 const resetBtn = document.querySelector('#resetBtn');
@@ -43,7 +46,7 @@ resetBtn.addEventListener('click', () => {
     data.length = 0;
     i = 0;
     elapsed = 0;
-    count = 1;
+    count = 0;
     tableContainer.innerHTML = '';
     activeColumns.fill(false);
     tableHeaders.forEach((val) => {
@@ -53,6 +56,10 @@ resetBtn.addEventListener('click', () => {
     });
     showChart1 = false;
     showChart2 = false;
+
+    bubbleOffset = 0;
+    lastBubbleY = null;
+    chartBtn.click();
 });
 
 const tableContainer = document.querySelector('#table tbody');
@@ -73,7 +80,6 @@ chartBtn.addEventListener('click', () => {
     if (!showCharts) {
         chartBtn.classList.add('button-clicked');
         activeColumns[3] = false;
-        // highlightColumn(3);
     } else {
         chartBtn.classList.remove('button-clicked');
         activeColumns[3] = true;
@@ -105,10 +111,10 @@ window.addEventListener('resize', () => {
 const _tube = () => {
     return {
         top: 50,
-        bottom: 900, //windowHeight - 2 * 50,
+        bottom: windowHeight - 100, // 900
         left: 70,
         right: 80,
-        height: 900, //windowHeight - 2 * 50,
+        height: windowHeight - 100, // 900
         middleH: 35 + (80 + 70) / 2,
     };
 };
@@ -134,6 +140,10 @@ function dataMotion(screenPos = 0, s = 0, x = 0, t = 0, deltaT = 0, V = 0) {
     };
 }
 
+function roundTo(x, step) {
+    return Math.round((x + Number.EPSILON) / step) * step;
+}
+
 function createPlots() {
     plot1 = new Plot(
         plotSize.plotPositionX,
@@ -151,21 +161,31 @@ function createPlots() {
     );
 
     if (activeColumns[1]) {
-        plot1.begin();
-        plot1.drawAxisX('t[s]', 12, 0, 30, 1);
-        plot1.drawAxisY('S[cm]', 12, 0, 120, 1);
         const plot1Points = data.filter((d) => d.t !== 0).map((d) => ({ x: d.t, y: d.s }));
-        plot1Points.unshift({ x: 0, y: 0 });
-        plot1.drawPlot(plot1Points, 0, 30, 0, 120);
+        plot1Points[0] = { x: 0, y: 0 };
+
+        const maxT = 12 * plot1Points[1]?.x ?? 0;
+
+        plot1.begin();
+        plot1.drawAxisX('t[s]', 12, 0, maxT, 1);
+        plot1.drawAxisY('S[cm]', 12, 0, 120, 1);
+        plot1.drawPlot(plot1Points, 0, maxT, 0, 120);
     }
 
     if (activeColumns[5]) {
+        const plot2Points = data
+            .filter((d) => d.t !== 0)
+            .map((d) => ({ x: d.t, y: d.x.toFixed(1) / d.deltaT.toFixed(1) }));
+        plot2Points[0] = { x: 0, y: data[2].x.toFixed(1) / data[2].deltaT.toFixed(1) };
+
+        const maxT = 12 * plot2Points[1]?.x ?? 0;
+        const maxV = plot2Points[1]?.y.toFixed(1) * 2 ?? 0;
+
         plot2.begin();
-        plot2.drawAxisX('t[s]', 12, 0, 30, 1);
-        plot2.drawAxisY('V[cm/s]', 5, 0, 10, 1);
-        const plot2Points = data.filter((d) => d.t !== 0).map((d) => ({ x: d.t, y: d.s / d.t }));
-        plot2Points.unshift({ x: 0, y: data[2].s / data[2].t });
-        plot2.drawPlot(plot2Points, 0, 30, 0, 10);
+        plot2.drawAxisX('t[s]', 12, 0, maxT, 1);
+        plot2.drawAxisY('V[cm/s]', 4, 0, maxV, 2);
+
+        plot2.drawPlot(plot2Points, 0, maxT, 0, maxV);
     }
 }
 
@@ -173,15 +193,15 @@ function createTable() {
     // here create table from data array
     tableContainer.innerHTML = '';
     data.forEach((val, ind) => {
-        if (startAnimation && (ind > 1) & (val.t !== 0)) {
+        if ((ind > 1) & (val.t !== 0)) {
             const tableRow = document.createElement('tr');
             tableRow.innerHTML = `
             <td>${ind - 1}</td>
             <td>${val.s}</td>
             <td>${val.x}</td>
-            <td>${val.t}</td>
-            <td>${val.deltaT}</td>
-            <td>${val.V}</td>
+            <td>${val.t.toFixed(3)}</td>
+            <td>${val.deltaT.toFixed(1)}</td>
+            <td>${(val.x.toFixed(1) / val.deltaT.toFixed(1)).toFixed(1)}</td>
         `;
             tableContainer.appendChild(tableRow);
         }
@@ -234,49 +254,132 @@ function drawTube() {
     pop();
 }
 
-function drawBubble() {
+function drawBubble(bubblePosY) {
     push();
     noStroke();
     fill(55, 164, 241, 160);
-    ellipse(tubeSize.middleH, tubeSize.top + tubeSize.bottom - 25 + i, tubeSize.right, 50);
+    ellipse(tubeSize.middleH, bubblePosY, tubeSize.right, 50);
     pop();
 }
 
 function draw() {
+    background(0);
+    drawTube();
+
+    const baseY = tubeSize.top + tubeSize.height - 25;
+    const bubbleY = baseY + bubbleOffset;
+
+    drawBubble(bubbleY);
+
+    fill(255);
+    textSize(25);
+    text(startTime ? `${(elapsed / 1000).toFixed(1)} s` : '0.0 s', 230, 50);
+
     if (startAnimation) {
-        if (data[1].screenPos === tubeSize.top + tubeSize.bottom - 50 + i) {
-            startTime = millis();
+        if (lastBubbleY === null) {
+            lastBubbleY = bubbleY;
         }
-        if (tubeSize.bottom - 25 + i > 25) {
-            i = i - 0.5;
-            if (startTime) {
-                elapsed = millis() - startTime;
-                if (elapsed - newTime > 100) {
-                    newTime = elapsed;
+
+        const dtSec = deltaTime / 1000;
+
+        const yBefore = lastBubbleY;
+
+        bubbleOffset -= speedPxPerSec * dtSec;
+
+        const yAfter = baseY + bubbleOffset;
+
+        // 1) set startTime in 0-line on scale
+        const startMarkY = data[1]?.screenPos + 25;
+
+        if (startTime == null && startMarkY != null) {
+            const crossedStart = yBefore > startMarkY && yAfter <= startMarkY;
+
+            if (crossedStart) {
+                const frac = (yBefore - startMarkY) / (yBefore - yAfter);
+
+                const now = millis();
+
+                startTime = now - (1 - frac) * deltaTime;
+
+                elapsed = 0;
+                newTime = 0;
+            }
+        }
+
+        // 2) if startTime is set, count elepsed
+        if (startTime != null) {
+            elapsed = millis() - startTime;
+        }
+
+        // 3) time messure on thick line on the scale
+        let previousValue = data[1];
+
+        for (const val of data) {
+            if (!val || val.screenPos == null) continue;
+
+            const markY = val.screenPos + 25;
+
+            const crossed = yBefore > markY && yAfter <= markY;
+
+            if (crossed && startTime != null) {
+                const frac = (yBefore - markY) / (yBefore - yAfter);
+
+                const tAtMarkMs = millis() - startTime - (1 - frac) * deltaTime;
+                const tAtMarkSec = tAtMarkMs / 1000;
+
+                if (!val.t || val.t === 0) {
+                    val.t = tAtMarkSec;
+
+                    val.s = 10 * count;
+                    count++;
+
+                    val.x = 10;
+
+                    const prevT = previousValue && previousValue.t ? previousValue.t : 0;
+                    val.deltaT = tAtMarkSec - prevT;
+
+                    val.V = val.deltaT > 0 ? val.x / val.deltaT : 0;
                 }
             }
 
-            data.forEach((val) => {
-                if (val.screenPos === tubeSize.top + tubeSize.bottom - 50 + i) {
-                    if (elapsed) {
-                        val.t = (elapsed / 1000).toFixed(1);
-                        val.s = 10 * count;
-                        count++;
-                        val.x = 10;
-                        val.deltaT = data[2].t;
-                        val.V = (val.s / val.t).toFixed(0);
-                    }
-                }
-            });
+            previousValue = val;
         }
-    }
-    background(0);
 
-    drawTube();
-    drawBubble();
-    fill(255);
-    textSize(25);
-    text(elapsed ? `${(elapsed / 1000).toFixed(1)} s` : '0.0 s', 230, 50);
+        lastBubbleY = yAfter;
+
+        if (yAfter <= tubeSize.top + 25) {
+            startAnimation = false;
+        }
+
+        // if (abs(data[1].screenPos - (tubeSize.top + tubeSize.bottom - 50 + i)) < 0.3) {
+        //     startTime = millis();
+        // }
+        // if (tubeSize.bottom - 25 + i > 25) {
+        //     i = i - 0.4;
+        //     if (startTime) {
+        //         elapsed = millis() - startTime;
+        //         if (elapsed - newTime > 100) {
+        //             newTime = elapsed;
+        //         }
+        //     }
+
+        //     let previousValue = data[1];
+        //     data.forEach((val) => {
+        //         if (abs(val.screenPos - (tubeSize.top + tubeSize.bottom - 50 + i)) < 0.3) {
+        //             if (elapsed) {
+        //                 val.t = (elapsed / 1000).toFixed(1);
+        //                 val.s = 10 * count;
+        //                 count++;
+        //                 val.x = 10;
+        //                 val.deltaT = (val.t - previousValue.t).toFixed(1); //data[2].t;
+        //                 val.V = (val.x / val.deltaT).toFixed(1);
+        //             }
+        //         }
+        //         previousValue = val;
+        //     });
+        // }
+    }
+
     if (showCharts) {
         createPlots();
     }
